7859db49b908b525ec78c180f196b7eef3edeb10
manya-s13 <manyasharma137@gmail.com>
2025-06-13 16:44:43 +0530

    final

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,21 +1,50 @@
 import React from "react";
+import { ExternalLink, Eye } from 'lucide-react'
 
-function ProjectCard({ title, description, dates, tags = [], image }) {
+function ProjectCard({ title, description, dates, tags, link, deploy = [], image }) {
   return (
-    <div className="flex flex-col border rounded-xl shadow-md overflow-hidden bg-zinc-900 text-violet-300 hover:shadow-lg transition-all duration-300">
+    <div className="flex flex-col rounded-xl shadow-xl overflow-hidden bg-zinc-900 text-gray-400 hover:text-blue-400 hover:scale-105 transition-all duration-300">
       {image && (
         <img
           src={image}
           alt={title}
-          className="h-40 w-full object-cover object-top"
+          className="h-50 w-full object-cover object-top"
         />
       )}
-      <div className="p-4 flex flex-col justify-between h-full">
+      <div className="p-4 flex flex-col justify-between h-full ">
         <div>
-          <h3 className="text-lg font-semibold">{title}</h3>
+          {/* <h3 className="text-lg font-semibold">{title}</h3> */}
+          <div className="flex justify-between items-center">
+        <h3 className="text-lg font-semibold">{title}</h3>
+        <div className="flex gap-3">
+          {/* Eye Icon Link */}
+          {deploy && (
+            <a
+              href={deploy}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-400 hover:text-blue-400 transition"
+            >
+              <Eye size={18} />
+            </a>
+          )}
+          {/* External Link Icon */}
+          {link && (
+            <a
+              href={link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-400 hover:text-blue-400 transition"
+            >
+              <ExternalLink size={18} />
+            </a>
+          )}
+        </div>
+      </div>
           <p className="text-xs text-gray-200">{dates}</p>
           <p className="mt-2 text-sm text-gray-200">{description}</p>
         </div>
+        
         {tags.length > 0 && (
           <div className="mt-3 flex flex-wrap gap-2">
             {tags.map((tag, idx) => (
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,30 +1,39 @@
 import React from 'react'
-import   ProjectCard   from './ProjectCard'
+import   ProjectCard from './ProjectCard'
+import {motion} from 'framer-motion'
 
 function Projects() {
 
     const projectData = [
+       
+      {
+        title: "Blogify",
+        description: "Site to write and read Blogs",
+        dates: "Nov 2024",
+        image: "/project2.png",
+        link: "https://github.com/manya-s13/blogify.git",
+        deploy: "",
+        tags: ["React", "NodeJS", "Tailwind", "MongoDB"],
+      },
+      
         {
           title: "CloudDocs",
-          description: "File Sharing and Storing Project",
+          description: "File Sharing and Storing App",
           dates: "Feb 2025",
           image: "/Project1.png",
+          link: "https://github.com/manya-s13/cloudDocs.git",
+          deploy: "",
           tags: ["NextJS", "Appwrite", "Tailwind", "ShadCN"],
         },
-          {
-            title: "ChatApp",
-            description: "Real Time Chat Application",
-            dates: "May 2025",
-            image: "/project3.png",
-            tags: ["React", "NodeJS", "Tailwind", "MongoDB"],
-          },
         {
-            title: "Blogify",
-            description: "Site to write and read Blogs",
-            dates: "Nov 2024",
-            image: "/project2.png",
-            tags: ["React", "NodeJS", "Tailwind", "MongoDB"],
-          },
+          title: "ChatApp",
+          description: "Real Time Chat Application",
+          dates: "May 2025",
+          image: "/project3.png",
+          link:  "https://github.com/manya-s13/ChatApp.git",
+          deploy: "",
+          tags: ["React", "NodeJS", "Tailwind", "MongoDB"],
+        },
          
       ];
 
@@ -34,6 +43,8 @@ function Projects() {
             description: "drift-ui-nine.vercel.app",
             dates: "May 2025",
             image: "/drift.png",
+            link: "https://github.com/manya-s13/DriftUI.git",
+            deploy: "https://drift-ui-nine.vercel.app",
             tags: ["React", "Tailwind", "Framer"],
           },
           {
@@ -41,16 +52,15 @@ function Projects() {
             description: "Landing Page",
             dates: "May 2025",
             image: "/drift.png",
+            link: "https://github.com/manya-s13/DriftUI.git",
+            deploy: "https://drift-ui-nine.vercel.app",
             tags: ["React", "Tailwind", "Framer"],
           }
       ]
 
   return (
     <div id='Projects' className='mt-50 mb-10'>
-        {/* <div className='flex items-center justify-center gap-4 font-semibold'>
-            <span className='text-5xl text-white'>My</span>
-            <span className='text-pink-300 text-5xl'>Projects</span>
-        </div> */}
+
          <div className='flex items-center justify-center gap-4 font-bold mb-5 animate-fade-in'>
           <span className='text-5xl text-white bg-gradient-to-r from-white to-gray-300 bg-clip-text text-transparent'>My</span>
           <span className='text-pink-400 text-5xl bg-gradient-to-r from-pink-400 to-purple-500 bg-clip-text text-transparent'>Projects</span>
@@ -63,7 +73,17 @@ function Projects() {
         </div>
         <div className="mt-10 px-8 md:px-36 grid gap-8 md:grid-cols-2 lg:grid-cols-3 ">
         {projectData.map((project, idx) => (
-          <ProjectCard key={idx} {...project} />
+
+          <motion.div
+          key={idx}
+          initial={{opacity:0 , y: 50 }}
+          whileInView={{opacity: 1, y:0}}
+          transition={{duration: 1, delay: idx * 0.5}}
+          viewport={{once: true}} >
+                    <ProjectCard key={idx} {...project} />
+          </motion.div>
+
+          
         ))}
       </div>
       <div className='text-center mt-15 font-semibold text-pink-300 text-xl'>
@@ -71,7 +91,15 @@ function Projects() {
         </div>
         <div className="mt-10 px-8 md:px-70 grid gap-8 md:grid-cols-1 lg:grid-cols-2 ">
         {frontendProjects.map((project, idx) => (
-          <ProjectCard key={idx} {...project} />
+                 <motion.div
+                 key={idx}
+                 initial={{opacity:0 , y: 50 }}
+                 whileInView={{opacity: 1, y:0}}
+                 transition={{duration: 1, delay: idx * 0.5}}
+                 viewport={{once: true}} >
+                           <ProjectCard key={idx} {...project} />
+                 </motion.div>
+       
         ))}
       </div>
     </div>
